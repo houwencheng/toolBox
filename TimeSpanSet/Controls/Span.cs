ecb24828277d9803369202ba8b828e65c97e699c
hwc <183855294@qq.com>
2020-06-07 23:46:51 +0800

    时间段选择控件wpf

---
+++ TimeSpanSet/Controls/Span.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TimeSpanSet.Controls
+{
+    /// <summary>
+    /// 长度条
+    /// </summary>
+    public class Span :System.Windows.Controls.Control
+    {
+
+        public double BeginValue
+        {
+            get { return (double)GetValue(BeginValueProperty); }
+            set{ SetValue(BeginValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for BeginValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty BeginValueProperty =
+            DependencyProperty.Register("BeginValue", typeof(double), typeof(Span), new PropertyMetadata(0.0));
+
+        public double EndValue
+        {
+            get { return (double)GetValue(EndValueProperty); }
+            set { SetValue(EndValueProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for EndValue.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty EndValueProperty =
+            DependencyProperty.Register("EndValue", typeof(double), typeof(Span), new PropertyMetadata(0.0));
+
+    }
+}
---
+++ TimeSpanSet/Controls/VAlueToTimeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeSpanSet.Controls
+{
+    public class ValueToTimeConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var data = (double)value;
+            var date = DateTime.Parse("00:00:00");
+            return date.AddMinutes(data).ToString("HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var para= (System.Windows.Data.RelativeSource)parameter;
+            var dateString = (string)value;
+            var date = new DateTime();
+            var ok = DateTime.TryParse(dateString, out date);
+            if (!ok) return 0.0;
+            var hour = date.Hour;
+            var minute = date.Minute;
+            var allMinute = hour * 60 + minute;
+            return allMinute;
+        }
+    }
+}
