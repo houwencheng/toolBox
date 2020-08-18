354ec571a879c718d83fccf009574132b7ba5939
houwencheng <houwencheng@cwddd.com>
2020-08-18 14:57:26 +0800

    增加Tool项目

---
+++ Tool/LogHelper.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool
+{
+    /// <summary>
+    /// 日志帮助类
+    /// </summary>
+    public class LogHelper
+    {
+        /// <summary>
+        /// 静态只读实例
+        /// </summary>
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(LogHelper));
+
+        static LogHelper()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        /// <summary>
+        /// 记录错误日志
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exp"></param>
+        public static void LogError(object message, Exception exp)
+        {
+            log.Error(LogFormat(message), exp);
+        }
+
+        /// <summary>
+        /// 记录错误日志
+        /// </summary>
+        /// <typeparam name="T">调用日志的类型</typeparam>
+        /// <param name="message"></param>
+        public static void LogError(object message)
+        {
+            log.Error(LogFormat(message));
+        }
+
+        /// <summary>
+        /// 记录警告日志
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exp"></param>
+        public static void LogWarn(object message, Exception exp)
+        {
+            log.Warn(LogFormat(message), exp);
+        }
+
+        /// <summary>
+        /// 记录警告日志
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogWarn(object message)
+        {
+            log.Warn(LogFormat(message));
+        }
+
+
+        /// <summary>
+        /// 记录信息日志
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exp"></param>
+        public static void LogInfo(object message, Exception exp)
+        {
+            log.Info(LogFormat(message), exp);
+        }
+
+        /// <summary>
+        /// 记录信息日志
+        /// </summary>
+        /// <param name="message"></param>
+        public static void LogInfo(object message)
+        {
+            log.Info(LogFormat(message));
+        }
+
+        /// <summary>
+        /// 日志格式
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private static string LogFormat(object message)
+        {
+            var method = GetStackMethodFullName(3);
+            var msg = string.Format("{0} - {1}", method, message);
+            return msg;
+        }
+
+        /// <summary>
+        /// 获取调用堆栈方法名称
+        /// </summary>
+        /// <param name="depth">层数</param>
+        /// <returns></returns>
+        private static string GetStackMethodFullName(int depth)
+        {
+            try
+            {
+                System.Diagnostics.StackTrace st = new System.Diagnostics.StackTrace();
+                string methodName = st.GetFrame(depth).GetMethod().Name;
+                string className = st.GetFrame(depth).GetMethod().DeclaringType.ToString();
+                return className + "." + methodName;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 改变默认的日志位置
+        /// </summary>
+        /// <param name="folder"></param>
+        public static void UpdateFolder(string folder)
+        {
+            var repository = log4net.LogManager.GetRepository();
+            var appenders = repository.GetAppenders();
+            if (appenders == null) return;
+            foreach (var appender in appenders)
+            {
+                if (appender.GetType().Equals(typeof(log4net.Appender.RollingFileAppender)))
+                {
+                    var ra = appender as log4net.Appender.RollingFileAppender;
+                    ra.File = folder;
+                    ra.ActivateOptions();
+                }
+            }
+        }
+    }
+}
---
+++ Tool/WebAPIHelper.cs
@@ -0,0 +1,140 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool
+{
+    /// <summary>
+    /// 用来调用WebAPI
+    /// </summary>
+    public static class WebAPIHelper
+    {
+        private static HttpClientHandler GetHttpClientHandler()
+        {
+            return new HttpClientHandler() { AutomaticDecompression = DecompressionMethods.GZip };
+        }
+
+        public static string Get(string url, string token = null)
+        {
+            var handler = GetHttpClientHandler();
+            using (var http = new HttpClient(handler))
+            {
+                if (token != null)
+                {
+                    http.DefaultRequestHeaders.Add("access-token", token);
+                    http.DefaultRequestHeaders.Add("Authorization", token);
+                }
+
+                var request = http.GetAsync(url);
+                var response = request.Result;
+
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                return responseString;
+            }
+        }
+
+        public static string Get(string url, dynamic obj, string token = null)
+        {
+            var handler = GetHttpClientHandler();
+            using (var http = new HttpClient(handler))
+            {
+                if (token != null)
+                {
+                    http.DefaultRequestHeaders.Add("access-token", token);
+                    http.DefaultRequestHeaders.Add("Authorization", token);
+                }
+
+                var request = http.GetAsync(url);
+
+                var response = request.Result;
+
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                return responseString;
+            }
+        }
+
+
+        public static string Post(string url, dynamic obj, string token = null)
+        {
+            var handler = GetHttpClientHandler();
+            using (var http = new HttpClient(handler))
+            {
+                if (token != null)
+                {
+                    http.DefaultRequestHeaders.Add("access-token", token);
+                    http.DefaultRequestHeaders.Add("Authorization", token);
+                }
+
+                var json = JsonConvert.SerializeObject(obj);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var request = http.PostAsync(url, content);
+                var response = request.Result;
+
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                return responseString;
+            }
+        }
+
+        public static string PostForm<T>(string url, T value, string token = null)
+        {
+            var modelType = typeof(T);
+            var handler = GetHttpClientHandler();
+            using (var http = new HttpClient(handler))
+            using (var formData = new MultipartFormDataContent())
+            {
+                foreach (var item in modelType.GetProperties())
+                {
+                    var obj = item.GetValue(value);
+                    if (obj != null)
+                    {
+                        var objString = obj.ToString();
+                        HttpContent content = new StringContent(objString, Encoding.UTF8, "application/json");
+                        formData.Add(content, item.Name);
+                    }
+                }
+
+                if (token != null)
+                {
+                    http.DefaultRequestHeaders.Add("access-token", token);
+                    http.DefaultRequestHeaders.Add("Authorization", token);
+                }
+                var request = http.PostAsync(url, formData);
+                var response = request.Result;
+
+                var responseString = response.Content.ReadAsStringAsync().Result;
+                return responseString;
+            }
+        }
+
+        public static string PostFile(string url, string fileFullName, string token = null)
+        {
+            var handler = GetHttpClientHandler();
+            using (var fileStream = File.OpenRead(fileFullName))
+            {
+                using (var http = new HttpClient(handler))
+                {
+                    if (token != null) http.DefaultRequestHeaders.Add("access-token", token);
+                    using (var content = new MultipartFormDataContent())
+                    {
+                        var fileContent = new StreamContent(fileStream);
+                        var fileName = Path.GetFileName(fileFullName);
+                        fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment") { FileName = fileName };
+                        content.Add(fileContent);
+                        var request = http.PostAsync(url, content);
+                        var response = request.Result;
+
+                        var responseString = response.Content.ReadAsStringAsync().Result;
+                        return responseString;
+                    }
+                }
+            }
+        }
+    }
+}
