d25feaac5c48635833d880a430338f29da2ec93c
houwencheng <houwencheng@cwddd.com>
2020-09-27 12:02:37 +0800

    时间线优化

--- WpfControls/PlayTimeLine.xaml.cs
+++ WpfControls/PlayTimeLine.xaml.cs
@@ -17,6 +17,7 @@ namespace WpfControls
 {
     /// <summary>
     /// PlayTimeLine.xaml 的交互逻辑
+    /// *Mark是刻度的意思
     /// </summary>
     public partial class PlayTimeLine : UserControl
     {
@@ -24,34 +25,28 @@ namespace WpfControls
         {
             InitializeComponent();
             canvas.SizeChanged += Canvas_SizeChanged;
-            grid.MouseWheel += MarkGrid_MouseWheel;
+            grid.MouseMove += Grid_MouseMove;
+            grid.MouseWheel += Grid_MouseWheel;
+            grid.SizeChanged += Grid_SizeChanged;
+
+            leftValue = (DateTime.Now.Date.AddDays(-1) - baseTime).TotalSeconds;
+            playPosition = leftValue;
+            UpdateMarkUnitTxt();
+
+            SimuPlay();
         }
 
-
-        #region 时间线和容器控制
+        /// <summary>
+        /// 左值 Unix时间戳
+        /// </summary>
+        private double leftValue;
 
         /// <summary>
-        /// 时间线和外容器的长度比率，应大于1
+        /// 播放位置(Unix时间戳)
         /// </summary>
-        private double _ratio = 2;
+        double playPosition;
 
-        /// <summary>
-        /// 拖拽世间条时鼠标相对于控件的位置
-        /// </summary>
-        private Point _mousePosition;
-
-        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            var ratio = e.NewSize.Width / e.PreviousSize.Width;
-            var originLeft = (double)grid.GetValue(System.Windows.Controls.Canvas.LeftProperty);
-            var nowLeft = originLeft * ratio;
-            grid.SetValue(System.Windows.Controls.Canvas.LeftProperty, nowLeft);
-            grid.Width = canvas.ActualWidth * _ratio;
-            BuildMark();
-        }
-
-
-        private void DragTimeLine(object sender, MouseEventArgs e)
+        private void Grid_MouseMove(object sender, MouseEventArgs e)
         {
             var mousePosition = e.GetPosition(this);
             var mouseLeftButtonDown = e.LeftButton == MouseButtonState.Pressed;
@@ -69,6 +64,62 @@ namespace WpfControls
             _mousePosition = mousePosition;
         }
 
+        private void Canvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var ratio = e.NewSize.Width / e.PreviousSize.Width;
+            var originLeft = (double)grid.GetValue(System.Windows.Controls.Canvas.LeftProperty);
+            var nowLeft = originLeft * ratio;
+            grid.SetValue(System.Windows.Controls.Canvas.LeftProperty, nowLeft);
+            grid.Width = canvas.ActualWidth * _ratio;
+        }
+
+        private void Grid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var tempIndex = _markSpanPixesIndex;
+            if (e.Delta > 0)
+            {
+                if (_markSpanPixesIndex < _markSpanPixesList.Count - 1)
+                {
+                    _markSpanPixesIndex++;
+                }
+            }
+            else
+            {
+                if (_markSpanPixesIndex > 0)
+                {
+                    _markSpanPixesIndex--;
+                }
+            }
+
+            UpdateMarkUnitTxt();
+            if (tempIndex != _markSpanPixesIndex)
+                Refresh();
+
+        }
+
+        /// <summary>
+        /// 基址时间
+        /// </summary>
+        private DateTime baseTime = DateTime.Parse("1970/01/01");
+
+        #region 时间线和容器控制
+
+        /// <summary>
+        /// 时间线和外容器的长度比率，应大于1
+        /// </summary>
+        private double _ratio = 2;
+
+        /// <summary>
+        /// 拖拽世间条时鼠标相对于控件的位置
+        /// </summary>
+        private Point _mousePosition;
+
+
         /// <summary>
         /// 加载左边的时间条
         /// </summary>
@@ -78,7 +129,13 @@ namespace WpfControls
             // 相左加载的宽度
             var lefterWidth = widthDiff / 2;
             grid.SetValue(Canvas.LeftProperty, -lefterWidth);
-            BuildMark();
+
+            var unitValue = _precisionList[_markSpanPixesIndex];
+            var unitPixes = _markSpanPixesList[_markSpanPixesIndex];
+            var ratio = unitValue / unitPixes;
+            leftValue -= lefterWidth * ratio;
+
+            Refresh();
         }
 
         /// <summary>
@@ -90,7 +147,11 @@ namespace WpfControls
             // 相右加载的宽度
             var righterWidth = widthDiff / 2;
             grid.SetValue(Canvas.LeftProperty, -righterWidth);
-            BuildMark();
+            var unitValue = _precisionList[_markSpanPixesIndex];
+            var unitPixes = _markSpanPixesList[_markSpanPixesIndex];
+            var ratio = unitValue / unitPixes;
+            leftValue += righterWidth * ratio;
+            Refresh();
         }
 
         #endregion
@@ -117,10 +178,13 @@ namespace WpfControls
             30 * 60,
             60 * 60,
             2* 60 * 60};
+        /// <summary>
+        /// 单位刻度像素宽度列表
+        /// </summary>
         List<double> _markSpanPixesList = new List<double> { 3, 5, 7, 9, 10, 13 };
 
         /// <summary>
-        /// 使用的刻度小标
+        /// 使用的刻度下标
         /// </summary>
         int _markSpanPixesIndex = 2;
 
@@ -128,6 +192,12 @@ namespace WpfControls
         /// 等级列表
         /// </summary>
         private List<double> _levelList = new List<double> { 1, 2, 4, 8 };
+
+        /// <summary>
+        /// 总的刻度数
+        /// </summary>
+        private int markCount;
+
 
 
         /// <summary>
@@ -154,12 +224,19 @@ namespace WpfControls
         /// <summary>
         /// 生成刻度
         /// </summary>
+        private void Refresh()
+        {
+            BuildMark();
+            if (_recordTimeSpan != null && _recordTimeSpan.Count > 0)
+                BuildVieoRecord();
+        }
+
         private void BuildMark()
         {
             markGrid.Children.Clear();
             var markSpanPixes = _markSpanPixesList[_markSpanPixesIndex];
-            var count = markGrid.ActualWidth / markSpanPixes + 1;
-            for (int i = 0; i < count; i++)
+            markCount = (int)(markGrid.ActualWidth / markSpanPixes) + 1;
+            for (int i = 0; i < markCount; i++)
             {
                 Point startPoint = new Point { X = i * markSpanPixes, Y = markGrid.ActualHeight };
                 Point endPoint = new Point();
@@ -184,36 +261,21 @@ namespace WpfControls
                 Path path = new Path();
                 path.StrokeThickness = 1;
                 path.Data = lineGeometry;
-                path.DataContext = DateTime.Now;
                 path.Stroke = Brushes.Black;
-                path.ToolTip = new TextBlock { Text = string.Format("Num.{0}", i), Foreground = path.Stroke };
+                var markValue = leftValue + _precisionList[_markSpanPixesIndex] * i;
+                var markDateTime = baseTime.AddSeconds(markValue);
+                path.DataContext = markDateTime;
+                path.ToolTip = new TextBlock { Text = string.Format("Num.{0}@{1}", i, markDateTime), Foreground = path.Stroke };
                 markGrid.Children.Add(path);
             }
         }
 
-
-        private void MarkGrid_MouseWheel(object sender, MouseWheelEventArgs e)
+        /// <summary>
+        /// 更新刻度单位提示
+        /// </summary>
+        private void UpdateMarkUnitTxt()
         {
-            var tempIndex = _markSpanPixesIndex;
-            if (e.Delta > 0)
-            {
-                if (_markSpanPixesIndex < _markSpanPixesList.Count - 1)
-                {
-                    _markSpanPixesIndex++;
-                }
-            }
-            else
-            {
-                if (_markSpanPixesIndex > 0)
-                {
-                    _markSpanPixesIndex--;
-
-                }
-            }
-
-            if (tempIndex != _markSpanPixesIndex)
-                BuildMark();
-
+            unitTxt.Text = string.Format("{0}秒", _precisionList[_markSpanPixesIndex]);
         }
 
         #endregion
@@ -241,9 +303,71 @@ namespace WpfControls
         {
             recordGrid.Children.Clear();
 
+            var x = 0d;
+            var width = 30d;
+            var y = 0d;
+            var height = recordGrid.ActualHeight;
+            var maxWidth = recordGrid.ActualWidth;
 
+            var unitValue = _precisionList[_markSpanPixesIndex];
+            var unitPixes = _markSpanPixesList[_markSpanPixesIndex];
+            var ratio = unitPixes / unitValue;
+            for (int i = 0; i < _recordTimeSpan.Count; i++)
+            {
+                var record = _recordTimeSpan[i];
+                var begin = record[0];
+                x = (begin - leftValue) * ratio;
+
+                var end = record[1];
+                if (end > begin)
+                {
+                    width = (end - begin) * ratio;
+                }
+                else
+                {
+                    width = maxWidth - x;
+                }
+
+                if (x + width < 0) continue;
+                if (x > maxWidth) continue;
+
+                System.Windows.Media.RectangleGeometry rectangleGeometry = new RectangleGeometry();
+                rectangleGeometry.Rect = new Rect { X = x, Y = y, Height = height, Width = width };
+
+                Path path = new Path();
+                path.Fill = Brushes.Green;
+                path.Data = rectangleGeometry;
+                recordGrid.Children.Add(path);
+            }
         }
 
+        #endregion
+
+        #region 播放刻度
+
+        /// <summary>
+        /// 模拟播放
+        /// </summary>
+        public void SimuPlay()
+        {
+            Task.Run(() =>
+            {
+                var times = 1;
+                while (times++ < 60 * 60)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    playPosition++;
+                    Dispatcher.Invoke(() =>
+                    {
+                        var left = times * _markSpanPixesList[_markSpanPixesIndex] / _precisionList[_markSpanPixesIndex];
+                        playBorder.Margin = new Thickness { Left = left };
+                        var playDateTime = baseTime.AddSeconds(playPosition);
+                        playBorder.DataContext = playDateTime;
+                        playBorder.ToolTip = new TextBlock { Text = string.Format("Num.{0}@{1}", times, playDateTime), Foreground = playBorder.Background };
+                    });
+                }
+            });
+        }
         #endregion
     }
 }
