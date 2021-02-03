8b0cb17738f60bca815ad5476b210ec1d5f5b1df
houwencheng <houwencheng@cwddd.com>
2021-02-03 16:03:46 +0800

    M

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,21 +7,57 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    delegate int SumDelegate(int x, int y);
+
+    public static class Logger
+    {
+        public static Action<string> WriteMessage;
+
+        public static int LogLevel { get; set; } = 1;
+
+        public static void LogMessage(int s, string component, string msg)
+        {
+            if (s < LogLevel)
+                return;
+
+            var outputMsg = $"{DateTime.Now}\t{s}\t{component}\t{msg}";
+            WriteMessage(outputMsg);
+        }
+    }
+
     class Program
     {
+        private static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
         //[STAThread]
         [MTAThread]
         static void Main(string[] args)
         {
+            var d1 = new { X = 1, Y = 2 };
+            
+            //SumDelegate sumDelegate = new SumDelegate(Sum);
+            //List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //var jishu = GetJishu(list);
+            //foreach (var item in jishu)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
-            UnitTestProject1.NTPClientTest nTPClientTest = new UnitTestProject1.NTPClientTest();
-            nTPClientTest.TestMethod1();
-            Console.ReadLine();
+            //Logger.WriteMessage += LogMessage;
+
+            //UnitTestProject1.NTPClientTest nTPClientTest = new UnitTestProject1.NTPClientTest();
+            //nTPClientTest.TestMethod1();
+            //Console.ReadLine();
+            //return;
+            InterFace.IProgramRun programRun = new TuplesTest();
+            //programRun = new MD5Example();
+            //programRun = new CpuTestExample();
+            programRun = new OutShort();
+            programRun.Run();
             return;
-            //InterFace.IProgramRun programRun = new ThreadExample();
-            ////programRun = new MD5Example();
-            ////programRun = new CpuTestExample();
-            //programRun.Run();
 
             //WpfApp wpfApp = new WpfApp();
             //wpfApp.Run();
@@ -40,6 +76,36 @@ namespace ConsoleApp1
             thread.Start();
 
             Console.Read();
+        }
+
+        private static void LogMessage(string msg)
+        {
+            try
+            {
+                //using (var log = File.AppendText(logPath))
+                //{
+                //    log.WriteLine(msg);
+                //    log.Flush();
+                //}
+            }
+            catch (Exception)
+            {
+                // Hmm. We caught an exception while
+                // logging. We can't really log the
+                // problem (since it's the log that's failing).
+                // So, while normally, catching an exception
+                // and doing nothing isn't wise, it's really the
+                // only reasonable option here.
+            }
+        }
+
+        private static IEnumerable<int> GetJishu(List<int> list)
+        {
+            foreach (var item in list)
+            {
+                if (item % 2 == 1)
+                    yield return item;
+            }
         }
 
         private static void Window_Closed(object sender, EventArgs e)
---
+++ ConsoleApp1/TuplesTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// 元组测试
+    /// </summary>
+    public class TuplesTest : InterFace.IProgramRun
+    {
+
+        private static (string, int, double, string) QueryCityData(string name)
+        {
+            if (name == "New York City")
+                return (name, 8175133, 468.48, "w ");
+
+            return ("", 0, 0, "334");
+        }
+
+        public void Run()
+        {
+            var result = QueryCityData("New York City");
+
+            var city = result.Item1;
+            var pop = result.Item2;
+            var size = result.Item3;
+            var nothing = result.Item4;
+            Console.WriteLine(city);
+            Console.WriteLine(pop);
+            Console.WriteLine(size);
+        }
+    }
+}
